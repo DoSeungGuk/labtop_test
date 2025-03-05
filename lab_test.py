03c3e0ed144a258422c7465a71a4e8bbeddbaabb
DoSeungGuk <dororo737@gmail.com>
2025-03-06 04:56:08 +0900

    키 종류 추가, 수정 필요

--- lab_test.py
+++ lab_test.py
@@ -113,44 +113,72 @@ def register_raw_input(hwnd):
 
 # 가상 키 코드 -> 문자열
 VK_MAPPING = {
-    0x30: "0",
-    0x31: "1",
-    0x32: "2",
-    0x33: "3",
-    0x34: "4",
-    0x35: "5",
-    0x36: "6",
-    0x37: "7",
-    0x38: "8",
-    0x39: "9",
-    0x51: "Q",
-    0x57: "W",
-    0x45: "E",
-    0x52: "R",
-    0x54: "T",
-    0x59: "Y",
-    0x55: "U",
-    0x49: "I",
-    0x4F: "O",
-    0x50: "P",
-    0x41: "A",
-    0x53: "S",
-    0x44: "D",
-    0x46: "F",
-    0x47: "G",
-    0x48: "H",
-    0x4A: "J",
-    0x4B: "K",
-    0x4C: "L",
-    0x0D: "ENTER",
+    # 주키(문자열)
+    0x30: "0",   0x31: "1",   0x32: "2",   0x33: "3",   0x34: "4",
+    0x35: "5",   0x36: "6",   0x37: "7",   0x38: "8",   0x39: "9",
+    0x41: "A",   0x42: "B",   0x43: "C",   0x44: "D",   0x45: "E",
+    0x46: "F",   0x47: "G",   0x48: "H",   0x49: "I",   0x4A: "J",
+    0x4B: "K",   0x4C: "L",   0x4D: "M",   0x4E: "N",   0x4F: "O",
+    0x50: "P",   0x51: "Q",   0x52: "R",   0x53: "S",   0x54: "T",
+    0x55: "U",   0x56: "V",   0x57: "W",   0x58: "X",   0x59: "Y",
     0x5A: "Z",
-    0x58: "X",
-    0x43: "C",
-    0x56: "V",
-    0x42: "B",
-    0x4E: "N",
-    0x4D: "M",
-    0x20: "SPACE"
+
+    # 특수키 (Space, Enter, ESC, Tab 등)
+    0x20: "SPACE",
+    0x0D: "ENTER",
+    0x1B: "ESC",
+    0x09: "TAB",
+
+    # 기능키(F1~F12)
+    0x70: "F1",  0x71: "F2",  0x72: "F3",  0x73: "F4",
+    0x74: "F5",  0x75: "F6",  0x76: "F7",  0x77: "F8",
+    0x78: "F9",  0x79: "F10", 0x7A: "F11", 0x7B: "F12",
+
+    # 편집/제어키 (Insert, Delete, Home, End, PgUp, PgDn)
+    0x2D: "INS",
+    0x2E: "DEL",
+    0x24: "HOME",
+    0x23: "END",
+    0x21: "PGUP",
+    0x22: "PGDN",
+
+    # 방향키
+    0x25: "LEFT",
+    0x26: "UP",
+    0x27: "RIGHT",
+    0x28: "DOWN",
+
+    # Lock키 (CapsLock, NumLock, ScrollLock)
+    0x14: "CAPSLOCK",
+    0x90: "NUMLOCK",
+    0x91: "SCROLLLOCK",
+
+    # 숫자패드(Numpad)
+    0x60: "NUM0",
+    0x61: "NUM1",
+    0x62: "NUM2",
+    0x63: "NUM3",
+    0x64: "NUM4",
+    0x65: "NUM5",
+    0x66: "NUM6",
+    0x67: "NUM7",
+    0x68: "NUM8",
+    0x69: "NUM9",
+    0x6A: "NUM *",
+    0x6B: "NUM +",
+    0x6D: "NUM -",
+    0x6E: "NUM .",
+    0x6F: "NUM /",
+
+    # 기타 (PrintScreen, Pause/Break 등)
+    0x2C: "PRTSCR",
+    0x13: "PAUSE",
+
+    # 수정/조합키(Shift, Ctrl, Alt 등) - 일반적으로 누르면 VKey 이벤트가 뜨긴 하나,
+    # RawInput에서 활용시 눌렀다는 것만 인지 가능(문자 입력 X)
+    0x10: "SHIFT",
+    0x11: "CTRL",
+    0x12: "ALT",
 }
 
 # 새로운 WndProc 시그니처
@@ -170,14 +198,7 @@ class LaptopTestApp(tk.Tk):
         # 기타 테스트 관련 변수들
         self.disabled_hwids = []
         self.test_done = {
-            "키보드": False,
-            "디스플레이": False,
-            "카메라": False,
-            "마우스": False,
-            "Wi-Fi": False,
-            "USB": False,
-            "배터리": False,
-            "사운드": False
+            "키보드": False
         }
         self.test_status_labels = {}
 
@@ -215,16 +236,42 @@ class LaptopTestApp(tk.Tk):
     def open_keyboard_test(self):
         kb_window = tk.Toplevel(self)
         kb_window.title("키보드 테스트")
-        kb_window.geometry("600x300")
+        kb_window.geometry("1280x720")
         info_label = ttk.Label(kb_window, text="이 창에 포커스를 두고\n모든 키를 한 번씩 눌러보세요.\n완료 시 창이 닫힙니다.")
-        info_label.pack(pady=5)
+        info_label.pack(pady=8)
 
         keyboard_layout = [
-            ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"],
+            # 함수키 라인 (F1 ~ F12)
+            ["F1", "F2", "F3", "F4", "F5", "F6",
+            "F7", "F8", "F9", "F10", "F11", "F12"],
+
+            # 숫자열 & 편집 키 (INS, DEL, HOME, END, PGUP, PGDN)
+            ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0",
+            "INS", "DEL", "HOME", "END", "PGUP", "PGDN"],
+
+            # QWERTY 1
             ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"],
+            # QWERTY 2
             ["A", "S", "D", "F", "G", "H", "J", "K", "L", "ENTER"],
-            ["Z", "X", "C", "V", "B", "N", "M", "SPACE"]
+            # QWERTY 3
+            ["Z", "X", "C", "V", "B", "N", "M", "SPACE"],
+
+            # 방향키
+            ["UP", "LEFT", "DOWN", "RIGHT"],
+
+            # Lock 키들
+            ["CAPSLOCK", "NUMLOCK", "SCROLLLOCK"],
+
+            # 숫자패드
+            ["NUM7", "NUM8", "NUM9", "NUM /",
+            "NUM4", "NUM5", "NUM6", "NUM *",
+            "NUM1", "NUM2", "NUM3", "NUM -",
+            "NUM0", "NUM .", "NUM +"],
+
+            # 기타
+            ["ESC", "TAB", "SHIFT", "CTRL", "ALT", "PRTSCR", "PAUSE"]
         ]
+
         self.all_keys = set()
         self.key_widgets = {}
         for row_keys in keyboard_layout:
@@ -234,7 +281,7 @@ class LaptopTestApp(tk.Tk):
                 key_upper = key.upper()
                 self.all_keys.add(key_upper)
                 btn = tk.Label(
-                    row_frame, text=key, width=5, borderwidth=1, relief="solid",
+                    row_frame, text=key, width=10, borderwidth=1, relief="solid",
                     font=("Arial", 12), background="lightgray"
                 )
                 btn.pack(side="left", padx=3)
